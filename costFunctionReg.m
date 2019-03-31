5d02ae9e109cae0c3cc8fbe9b6e8a569405988ef
Alex G <gamezalexandra@live.com>
2019-03-31 14:45:58 -0500

    constFunctionReg

--- costFunctionReg.m
+++ costFunctionReg.m
@@ -16,10 +16,15 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+initial_theta = theta;
+
+h = sigmoid(X*theta);
 
 
+J = (1/m) *(-y'*log(h) - (1 - y)'*log(1 - h)) + (lambda/(2*m))*sum(theta(2:size(theta,1)).^2)
 
-
+reg_theta = [ initial_theta(1); (lambda/m)*(initial_theta(2:size(initial_theta,1))) ];
+grad = (1/m) * X'*(h - y) + reg_theta
 
 
 % =============================================================
